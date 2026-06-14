809390d38dbe6fc12bf70a690fb8ba7563904c5d
RobinHoo <53432684+robin0512@users.noreply.github.com>
2026-06-14 13:15:46 +0800

    Add shared assets and stylesheets to web/

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
+import "./index.css";
+import "./styles/styles.css";
+import "./styles/screens.css";
+import "./styles/login.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
-)
+);
